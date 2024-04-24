af8326286b6d8cb1f571472459e8eaa0c4dd1dcd
Areeba_Khan <98646330+44Areeba@users.noreply.github.com>
2024-04-25 01:37:42 +0500

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -47,8 +47,8 @@ const HomePage = () => {
       <div className="dark:bg-bgDark">
         <Hero
           address="225 S 1st St Brooklyn, NY 11211"
-          phone="(929) 123-4567"
-          email="constrctr@restate.com"
+          phone="+971502329456"
+          email="info@hrsdubai.com"
         />
 
         <About
